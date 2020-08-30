ddd543ff64101e59af724bbfa48f802eb10675bb
Chris Koerner <bowlerct@gmail.com>
2020-08-30 16:24:23 -0400

    fixed typos

--- tests/views/login_view_test.py
+++ tests/views/login_view_test.py
@@ -8,14 +8,14 @@ def test_redirect_to_login(client):
     response = client.get( reverse('index'), follow=False )
 
     assert response.status_code == 302
-    assert response["Location"] == reverse ('login')
+    assert response["Location"] == reverse('login')
 
 
-def test_index(client)
+def test_index(client):
     data = {
         username = 'cobbler',
         password = 'cobbler'
     }
 
-    #response = client.post('do_login', data=data)
+    response = client.post(reverse('do_login'), data)
     
