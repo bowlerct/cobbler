6331b05c64b90b454822b78ac035966fccf107aa
Chris Koerner <bowlerct@gmail.com>
2020-08-30 15:15:59 -0400

    Added tests for views

---
+++ tests/views/login_view_test.py
@@ -0,0 +1,11 @@
+import pytest
+
+# from django.conf.urls import url
+from django.urls import reverse
+
+
+def test_redirect_to_login(client):
+    response = client.get( reverse('index'), follow=False )
+
+    assert response.status_code == 302
+    assert response["Location"] = reverse ('login')
