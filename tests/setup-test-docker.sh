9888c6b25abe460682f0d4108262c87801f83ffc
Chris Koerner <bowlerct@gmail.com>
2020-08-30 16:49:59 -0400

    pytest-django

--- tests/setup-test-docker.sh
+++ tests/setup-test-docker.sh
@@ -26,9 +26,12 @@ zypper -n in make gzip sed git hg
 # Set tftpboot location correctly for SUSE distributions
 sed -e "s|/var/lib/tftpboot|/srv/tftpboot|g" -i cobbler/settings.py config/cobbler/settings
 
+# Install testing frameworks
+pip3 install pytest-django
+
 # Install and upgrade all dependecys
 pip3 install --upgrade pip
-pip3 install .[lint,test,pytest-django]
+pip3 install .[lint,test]
 
 # Install cobbler
 make install
