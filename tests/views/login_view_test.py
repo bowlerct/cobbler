5e83a6db238959c24ae28d5dc79eee10c799875f
Chris Koerner <bowlerct@gmail.com>
2020-08-31 13:29:15 -0400

    updated tests

--- tests/views/login_view_test.py
+++ tests/views/login_view_test.py
@@ -11,19 +11,20 @@ def test_redirect_to_login(client):
     assert 'login.tmpl' in (t.name for t in response.templates)
 
 
-def test_index(login_web):
-    client = login_web()
-    response = client.get( reverse('index') )
+def test_index(client):
+    # client = login_web()
+    # response = client.get( reverse('index') )
+    data = {'username': 'cobbler', 'password': 'cobbler'}
+    response = client.post(reverse('do_login'), data)
 
     assert response.status_code == 200
-    assert 'login.tmpl' in (t.name for t in response.templates)
     assert 'index.tmpl' in (t.name for t in response.templates)
     assert b'Welcome to <a href="https://cobbler.github.io/">Cobbler' in response.content
     assert 'cobbler' == response.context['username']
 
 
-def test_settings_list(client, login_web):
-    login_web()
+def test_settings_list(login_web):
+    client = login_web()
     response = client.get( reverse('setting_list') )
 
     assert 'settings.tmpl' in (t.name for t in response.templates)
