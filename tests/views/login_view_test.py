4d2253b80797958a2fd663a967a5d25a2d91f243
Chris Koerner <bowlerct@gmail.com>
2020-08-31 12:48:42 -0400

    updated tests

--- tests/views/login_view_test.py
+++ tests/views/login_view_test.py
@@ -1,8 +1,7 @@
 import pytest
 
-# from django.conf.urls import url
 from django.urls import reverse
-#from cobbler.web import urls
+from cobbler import api as cobbler_api
 
 
 def test_redirect_to_login(client):
@@ -15,7 +14,17 @@ def test_redirect_to_login(client):
 def test_index(client, login_web):
     login_web()
     response = client.get( reverse('index') )
-    
+
+    assert response.status_code == 200
     assert 'index.tmpl' in (t.name for t in response.templates)
     assert b'Welcome to <a href="https://cobbler.github.io/">Cobbler' in response.content
     assert 'cobbler' == response.context['username']
+
+
+# def test_settings_list(client, login_web):
+#     login_web()
+#     response = client.get( reverse('setting_list') )
+
+#     assert 'settings.tmpl' in (t.name for t in response.templates)
+    
+#     settings = cobbler_api.settings()
