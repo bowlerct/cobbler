fff9135a5dc6acd275236cd27a364392d19d72e9
Chris Koerner <bowlerct@gmail.com>
2020-09-02 14:10:45 -0400

    added kwargs stmt

--- tests/views/login_view_test.py
+++ tests/views/login_view_test.py
@@ -59,7 +59,7 @@ def test_snippet_list(login_web):
                                     "repo", "package", "mgmtclass", "file"])
 def test_generic_list(login_web, what):
     client = login_web()
-    response = client.get( reverse('what_list', {'what': what, 'page': '1'}) )
+    response = client.get( reverse('what_list', kwargs={'what': what, 'page': '1'}) )
 
     assert response.status_code == 200
     assert 'generic_list.tmpl' in (t.name for t in response.templates)
