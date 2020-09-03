c5874362e15620de06813b228ff0a33e8e72b477
Chris Koerner <bowlerct@gmail.com>
2020-09-03 15:19:39 -0400

    Added remaining HTTP GET urls to logon tests

--- tests/views/logon_view_test.py
+++ tests/views/logon_view_test.py
@@ -5,21 +5,30 @@ from django.urls import reverse
 
 # test authentication required
 
-@pytest.mark.parametrize("view", ["index", "setting_list", "aifile_list", "snippet_list",
-                                    "events", "import_prompt", "check"])
-def test_redirect_to_login_on_get(client, view):
-    response = client.get( reverse(view) )
+# HTTP GET urls
+
+@pytest.mark.parametrize("what", [("index",[]), ("setting_list",[]), ("aifile_list",[]),
+                            ("snippet_list",[]), ("events",[]), ("import_prompt",[]),
+                            ("check",[]), ("what_list", ["distro"]), ("what_list", ["profile"]),
+                            ("what_list", ["system"]), ("what_list", ["image"]), 
+                            ("what_list", ["repo"]), ("what_list", ["package"]),
+                            ("what_list", ["mgmtclass"]), ("what_list", ["file"]),
+                            ("snippet_list", []), ("aifile_list", []), ("setting_list", []),
+                            ("events",[]), ("events_log",["time"]), ("check", []),
+                            ("task_created", []), ("import_prompt", []),
+                            ("utils_random_mac", []), ("utils_random_mac_virttype", ["xenpv"])])
+def test_redirect_to_login_on_get(client, what):
+    view, args = what
+    response = client.get( reverse(view, args=args) )
 
     assert response.status_code == 200
     # not authenticated - ensure we are redirected to login page 
     assert 'login.tmpl' in (t.name for t in response.templates)
 
-# FIXME
-# what_list -> requires args=[what,1]
-# Add checks for POST methods as well
+# HTTPD POST urls
+# FIXME Add checks for POST methods to ensure login required
+# def test_redirect_to_login_on_post(client, what):
 
-# def test_redirect_to_login_on_post(client, view):
-#    pass
 
 # test login
 
