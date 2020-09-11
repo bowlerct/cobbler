e30d442828c4e196abcceb39e8c9c4cc89ab4c8c
Chris Koerner <bowlerct@gmail.com>
2020-09-11 17:06:13 -0400

    Updated view tests for item list

--- tests/views/list_items_view_test.py
+++ tests/views/list_items_view_test.py
@@ -3,6 +3,7 @@ import pytest
 from django.urls import reverse
 from cobbler import api as cobbler_api
 
+# Test listing of items
 
 # Settings
 
@@ -45,20 +46,21 @@ def test_snippet_list(login_web):
                                     "repo", "package", "mgmtclass", "file"])
 def test_generic_list(login_web, what):
     client, response = login_web( reverse('what_list', args=[what, 1]) )
-    #response = client.get( reverse('what_list', args=[what, 1]) )
 
+    # No test data to list so we just ensure the correct template is rendered
     assert response.status_code == 200
     assert 'generic_list.tmpl' in (t.name for t in response.templates)
     assert b'Name' in response.content
 
 
-# FIXME test actions 'check' and 'sync' first so there are logs for events check to succeed
-
-
 def test_events_list(login_web):
-    client, response = login_web( reverse('events') )
+    # Call 'sync' first so there are logs for events check to succeed
+    client, response = login_web( reverse('index') )
+    response = client.post( reverse('sync') )
+    # redirects to task_created
+    assert response.status_code == 302
 
+    response = client.get( reverse('events') )
     assert b'Events' in response.content
-
     # requires at least one event has occurred
-    # assert b'/cobbler_web/eventlog/' in response.content
+    assert b'/cobbler_web/eventlog/' in response.content
